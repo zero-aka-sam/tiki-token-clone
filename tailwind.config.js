ac810aeb8cab3187ab46a46ac44371ae1523e717
Sand004 <84405739+Sand004@users.noreply.github.com>
2021-07-07 15:11:39 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+    backgroundImage: theme => ({
+
+         'hero-pattern': "url('https://media.discordapp.net/attachments/842155059999408181/862303850902847488/fondo.jpg')",
+
+         'footer-texture': "url('/img/footer-texture.png')",
+        })
+    },
   },
   variants: {
     extend: {},
