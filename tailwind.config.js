b205ced77c9443432da198111c8f796209a9fd10
Sand004 <84405739+Sand004@users.noreply.github.com>
2021-07-08 00:45:59 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,25 @@ module.exports = {
          'footer-texture': "url('/img/footer-texture.png')",
         })
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      babypink: {
+        DEFAULT: '#FFC0CB',
+      },
+      pink: {
+        light: '#ff7ce5',
+        DEFAULT: '#ff49db',
+        dark: '#ff16d1',
+      },
+      gray: {
+        darkest: '#1f2d3d',
+        dark: '#3c4858',
+        DEFAULT: '#c0ccda',
+        light: '#e0e6ed',
+        lightest: '#f9fafc',
+      }
+    }
   },
   variants: {
     extend: {},
