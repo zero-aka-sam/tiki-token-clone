000b9281a865e13b017c9c7a2754e28370db3bb6
Sand004 <84405739+Sand004@users.noreply.github.com>
2021-07-07 16:34:09 +0200

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -38,7 +38,7 @@ export default function Navbar({setmodep ,setsidebarp, address,dispatch}) {
                 </div>
                 <div className="rounded relative w-full max-w-xl mr-6 focus-within:text-purple-500">
 
-                        <input type="text" className="rounded py-2 border block w-full text-sm focus:outline-none dark:text-gray-300 form-input leading-5 focus:border-gray-700 dark:border-gray-700 focus:shadow-outline-purple dark:focus:border-gray-600 dark:focus:shadow-outline-gray dark:bg-gray-700 pl-8 text-gray-700 focus:ring-2 focus:ring-gray-400"
+                        <input type="text" className="rounded py-2 border block w-full text-sm focus:outline-none dark:text-gray-300 form-input leading-5 focus:border-gray-700 dark:border-gray-700 focus:shadow-outline-purple dark:focus:border-gray-600 dark:focus:shadow-outline-gray dark:bg-red-600 pl-8 text-gray-400 focus:ring-2 focus:ring-gray-400"
                             placeholder="Paste your address here"
                             defaultValue={address}
                             onChange={(e)=> {loadDividends(e)}}
