157d1aabb2c0348d6ea5759588c7a2a549166c79
Sand004 <84405739+Sand004@users.noreply.github.com>
2021-07-07 16:53:40 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children, dispatch, address }) {
   return (
     <div className={modep ? "dark h-screen" : " h-screen"}>
       <div className="flex  bg-gray-50 dark:bg-hero-pattern ">
-        <div className={`z-30 flex-shrink-0  w-64  bg-white dark:bg-gray-800 fixed lg:block h-screen  lg:relative -mt-11 top-10 transition-all duration-1000  ${sidebarp?" lg:left-0 left-0 ":"lg:left-0 -left-96"}`}>
+        <div className={`z-30 flex-shrink-0  w-64  bg-white dark:bg-pink-800 fixed lg:block h-screen  lg:relative -mt-11 top-10 transition-all duration-1000  ${sidebarp?" lg:left-0 left-0 ":"lg:left-0 -left-96"}`}>
           <Aside setsidebarp={setsidebarp} />
         </div>
         <div className="flex flex-col flex-1 w-full h-screen ">
